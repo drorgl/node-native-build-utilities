173f0ff48b6ae5fc3a46158d8778c8026cd6126b
Dror Gluska <drorgl@yahoo.com>
2017-04-14 20:55:23 +0300

    cleanup dist folder

--- dist/accessors/npm-package-arg.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-// tslint:disable-next-line:no-var-requires
-var npa_parse = require("npm-package-arg");
-function npa(arg) {
-    return npa_parse(arg);
-}
-exports.npa = npa;
-//# sourceMappingURL=npm-package-arg.js.map
--- dist/tmp_test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var minimatch = require("minimatch");
-console.log(minimatch(".\\node_modules\\balanced-match\\.npmignore", ".\\node_modules\\**", { flipNegate: false, dot: true }));
-//# sourceMappingURL=tmp_test.js.map
